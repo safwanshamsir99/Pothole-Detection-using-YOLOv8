0bf3f4901a4e40358e81c14c88a6f3cd0973b868
Safwan Shamsir <safwan.shamsir@wdc.com>
2024-10-09 09:49:55 +0800

    fix: video name and video path

--- img_processing/img.py
+++ img_processing/img.py
@@ -3,10 +3,10 @@ import time
 import os
 
 cpt = 0
-maxFrames = 100 # if you want 5 frames only.
+maxFrames = 100 # if you want 100 frames only.
 
 count=0
-cap=cv2.VideoCapture('mycarplate.mp4')
+cap=cv2.VideoCapture('Video/pothole.mp4')
 while cpt < maxFrames:
     ret, frame = cap.read()
     if not ret:
@@ -17,7 +17,7 @@ while cpt < maxFrames:
     frame=cv2.resize(frame,(1080,500))
     # cv2.imshow("test window", frame) # show image in window
     os.makedirs("images", exist_ok=True)
-    cv2.imwrite(r"images\numberplate_%d.jpg" %cpt, frame)
+    cv2.imwrite(r"images\plothole_%d.jpg" %cpt, frame)
     time.sleep(0.01)
     cpt += 1
     if cv2.waitKey(5)&0xFF==27:
